0e550763fb1db404cbfc5428c6cc9d6095ca2fe6
Odin-max <meh84741@gmail.com>
2025-05-16 21:16:32 +0300

    1

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,36 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.PositiveSmallIntegerField()
+    seats_in_row = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    duration = models.IntegerField()
+    title = models.CharField(max_length=155)
+    description = models.TextField(max_length=1800)
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
 
     def __str__(self):
         return self.title
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,27 @@
-from django.urls import path
+from django.urls import path, include
 
-from cinema.views import movie_list, movie_detail
+from .views import (
+    GenreListCreateAPIView, GenreDetailAPIView,
+    ActorListCreateAPIView, ActorDetailAPIView,
+    CinemaHallViewSet, MovieViewSet
+)
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"cinema/cinema_halls", CinemaHallViewSet, basename="cinema_hall")
+router.register(r"cinema/movies", MovieViewSet, basename="movie")
+
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+
+    path("cinema/genres/", GenreListCreateAPIView.as_view(), name="genre-list-create"),
+    path("cinema/genres/<int:pk>/", GenreDetailAPIView.as_view(), name="genre-detail"),
+
+    path("cinema/actors/", ActorListCreateAPIView.as_view(), name="actor-list-create"),
+    path("cinema/actors/<int:pk>/", ActorDetailAPIView.as_view(), name="actor-detail"),
+
+    path("api/", include(router.urls)),
 ]
 
 app_name = "cinema"
