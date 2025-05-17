84fc240283559c03db1b7a77f5234a524218e4bf
Odin-max <meh84741@gmail.com>
2025-05-17 10:32:29 +0300

    3

--- cinema/views.py
+++ cinema/views.py
@@ -37,7 +37,7 @@ class GenreDetailAPIView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, pk):
         genre = self.get_object(pk)
